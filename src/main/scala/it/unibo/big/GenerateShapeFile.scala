6543cebd2e384aa9b3215180cb6d8e862d430acf
Chiara Forresi <chiara.forresi@studio.unibo.it>
2024-03-06 14:47:28 +0100

    first working version of generate shape file

--- src/main/scala/it/unibo/big/GenerateShapeFile.scala
+++ src/main/scala/it/unibo/big/GenerateShapeFile.scala
@@ -1,34 +1,58 @@
 package it.unibo.big
 
-import com.vividsolutions.jts.geom.{Coordinate, Geometry, Point, PrecisionModel}
-import org.apache.spark.sql.functions.{col, udf}
-import org.datasyslab.geosparksql.utils.{Adapter, GeoSparkSQLRegistrator}
 object SparkToShapefile extends App {
   import Utils._
+  import com.vividsolutions.jts.geom.{Coordinate, Geometry, GeometryCollection, Point, PrecisionModel}
+  import geotrellis.vector
+  import org.apache.spark.sql.functions.{col, udf}
+  import org.datasyslab.geospark.spatialRDD.SpatialRDD
+  import org.datasyslab.geosparksql.utils.{Adapter, GeoSparkSQLRegistrator}
+  import org.geotools.data.shapefile.shp.{ShapeType, ShapefileWriter}
 
-  GeoSparkSQLRegistrator.registerAll(sparkSession)
+  import java.io.File
+  import java.nio.channels.FileChannel
+  import java.nio.file.StandardOpenOption
+  import java.util
 
-  val caseInputDataset = Utils.readCaseInputData()
-  private def getGeom = udf((geom: String) => {
-    try {
-      val geometry = readGeometry(geom)
-      val p = geometry.geom.asInstanceOf[geotrellis.vector.Point]
-      new Point(new Coordinate(p.x, p.y), new PrecisionModel(), geometry.srid).asInstanceOf[Geometry]
-    } catch {
-      case _: Exception => null
-    }
-  })
-  val trapDf = caseInputDataset("traps")
+  writeShapeFile()
+
+  private def writeShapeFile(): Unit = {
+    GeoSparkSQLRegistrator.registerAll(sparkSession)
+
+    val caseInputDataset = Utils.readCaseInputData()
+
+    def getGeom = udf((geom: String) => {
+      try {
+        val geometry = readGeometry(geom)
+        val p = geometry.geom.asInstanceOf[vector.Point]
+        new Point(new Coordinate(p.x, p.y), new PrecisionModel(), geometry.srid).asInstanceOf[Geometry]
+      } catch {
+        case _: Exception => null
+      }
+    })
+
+    val trapDf = caseInputDataset("traps")
       .where(col("geometry").isNotNull).withColumn("geometry", getGeom(col("geometry")))
-  trapDf.printSchema()
-  //java.lang.String cannot be cast to
-  val spatialRDD = Adapter.toSpatialRdd(trapDf, "geometry")
-  val shapefileDf = Adapter.toDf(spatialRDD,sparkSession)
-  shapefileDf.printSchema()
-  shapefileDf.show()
-  // Save the Spatial RDD as a shapefile TODO not works
-  shapefileDf.write.format("shapefile").option("path", "output.shp").save()
-  //spatialRDD.saveAsGeoJSON("output.geojson")
-  // Save the Spatial RDD as a Shapefile*/
-  sparkSession.stop()
+
+    val spatialRDD: SpatialRDD[Geometry] = Adapter.toSpatialRdd(trapDf, "geometry")
+
+    val shapefileWriter = new ShapefileWriter(
+      FileChannel.open(new File("output.shp").toPath, StandardOpenOption.CREATE, StandardOpenOption.WRITE),
+      FileChannel.open(new File("output.shx").toPath, StandardOpenOption.CREATE, StandardOpenOption.WRITE)
+    )
+
+    val records: util.List[Geometry] = spatialRDD.rawSpatialRDD.collect()
+    var geometries = Seq[Geometry]()
+    //iter
+    val iter = records.iterator()
+    while (iter.hasNext) {
+      val row: Geometry = iter.next()
+      geometries :+= row
+    }
+    val geometryCollection = new GeometryCollection(geometries.toArray, new PrecisionModel(), 4326)
+    shapefileWriter.write(geometryCollection, ShapeType.POINT)
+    shapefileWriter.close()
+
+    sparkSession.stop()
+  }
 }
