dc7bb6884546f3dd08ad1526b6b66cdbfec3dc5a
Chiara Forresi <chiara.forresi@studio.unibo.it>
2024-03-08 17:34:59 +0100

    refactor and work in reading weather shp

--- src/main/scala/it/unibo/big/normalized_fact/WeatherReader.scala
+++ src/main/scala/it/unibo/big/normalized_fact/WeatherReader.scala
@@ -1,35 +1,31 @@
 package it.unibo.big.normalized_fact
 
-import it.unibo.big.Utils.{config, sparkSession}
-import org.datasyslab.geospark.formatMapper.shapefileParser.ShapefileReader
-import org.datasyslab.geosparksql.utils.Adapter
-import org.slf4j.{Logger, LoggerFactory}
-import java.net.URL
-import java.io.File
-import org.apache.commons.io.FileUtils
-
 object WeatherReader extends App {
+  import it.unibo.big.Utils.config
+  import org.apache.commons.io.FileUtils
+  import org.datasyslab.geospark.formatMapper.shapefileParser.ShapefileReader
+  import org.datasyslab.geosparksql.utils.Adapter
+
+  import java.io.File
+  import java.net.URL
+
   def readWeatherShapefile(path: String) : Unit = {
     import it.unibo.big.Utils._
-    val tmp_folder = "tmp/"
+    val tmpFolder = "tmp/weather"
     val fileNames = getFileAndFolderNames(path)
     //download all files from path and save in the temporary folder (if not already present)
     fileNames.foreach(x => {
-      val tmpFile = new File(s"$tmp_folder$x")
+      val tmpFile = new File(s"$tmpFolder/$x")
       if(!tmpFile.exists()) {
         FileUtils.copyURLToFile(new URL(path + x), tmpFile)
       }
     })
-    //a .shp file is needed otherwise an exception is thrown
-    val shapefileTmpPath = fileNames.filter(_.endsWith(".shp")).map(tmp_folder + _)
-      .headOption.getOrElse(throw new Exception(s"No shapefile found in $path"))
 
-    // Read the shapefile into a Spark DataFrame
-    val shapefileRDD = ShapefileReader.readToPointRDD(sparkSession.sparkContext, shapefileTmpPath)
-    val df = Adapter.toDf(shapefileRDD, sparkSession)
+    val spatialRDD = ShapefileReader.readToGeometryRDD(sparkSession.sparkContext, tmpFolder)
+    val df = Adapter.toDf(spatialRDD, sparkSession)
     df.printSchema()
     df.show()
   }
-  //TODO check work
+  //TODO check error hadoop, remove geom column (?)
   readWeatherShapefile(config.getString("dataset.weather"))
 }
