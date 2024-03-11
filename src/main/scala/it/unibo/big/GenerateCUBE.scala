77af400cdf29a78c7360279088eb135fd6895e9d
Chiara Forresi <chiara.forresi@studio.unibo.it>
2024-03-11 16:24:48 +0100

    update code for main branch

--- src/main/scala/it/unibo/big/GenerateCUBE.scala
+++ src/main/scala/it/unibo/big/GenerateCUBE.scala
@@ -7,7 +7,6 @@ object GenerateCUBE extends App {
   import it.unibo.big.casedimension.{GenerateCaseDimensionTable, GenerateTrapDimension}
   import it.unibo.big.environment_registry.GenerateEnvironmentRegistryDimensionTables
   import it.unibo.big.normalized_fact.{WeatherReader, GenerateNormalizedFactWithMeteo, GenerateTrapsValidity}
-  import it.unibo.big.service.Postgres
   import org.apache.spark.sql.DataFrame
   import org.slf4j.{Logger, LoggerFactory}
   import it.unibo.big.ImagesMap.generateImagesMap
@@ -20,20 +19,13 @@ object GenerateCUBE extends App {
     //read case input data and create a temp view for each file
     val caseInputData: Map[String, DataFrame] = Utils.readInputData("CASE")
 
-    val postgres = new Postgres(sparkSession, "cimice")
-    val environmentRegistryInputData = Map("water_basin" -> postgres.queryTable("select d_ty_sda as type_name, geom4326 from acqueinterne"),
-      "water_course" -> postgres.queryTable("select prenome as praenomen, uso as usage, tombinato as culverted, geom4326 from retebonifica"),
-      "crop" -> postgres.queryTable("select raggruppam as crop_type, geom_4326 as geom4326 from uso_suolo"))
-
-   //TODO change val environmentRegistryInputData = Utils.readInputData("environment_registry")
+    val environmentRegistryInputData = Utils.readInputData("environment_registry")
 
     val link = config.getString("dataset.satellite_images")
 
     // download the images from link
     val mapImagesSVP : Map[String, Array[String]] = generateImagesMap(link)
-    val weatherDf : DataFrame = WeatherReader.readWeatherShapefile(config.getString("dataset.weather")) //TODO check if works on a cluster
-    val calculateSVP = false //TODO set to true
-
+    val weatherDf : DataFrame = WeatherReader.readWeatherShapefile(config.getString("dataset.weather"))
     //generate the normalized fact table
     val normalizedFinalDf: DataFrame = GenerateNormalizedFactWithMeteo(sparkSession, caseInputData, weatherDf)
     //generate the traps validity table
@@ -41,13 +33,11 @@ object GenerateCUBE extends App {
     //generate the trap dimension table
     var trapsDimensionTable : DataFrame = GenerateTrapDimension(sparkSession, caseInputData)
     trapsDimensionTable = trapsDimensionTable.join(trapsValidityDf, Seq("gid"))
-    if(calculateSVP) {
-      //calculate the automatic SVP
-      val automaticSVP = SVPStatistics.calculateAutomaticSVP(sparkSession, caseInputData ++ environmentRegistryInputData, trapRadius = 200, mapImagesSVP)
+    //calculate the automatic SVP
+    val automaticSVP = SVPStatistics.calculateAutomaticSVP(sparkSession, caseInputData ++ environmentRegistryInputData, trapRadius = 200, mapImagesSVP)
 
-      //join the automatic svp with the trap dimension
-      trapsDimensionTable = trapsDimensionTable.join(automaticSVP, Seq("gid"), "left")
-    }
+    //join the automatic svp with the trap dimension
+    trapsDimensionTable = trapsDimensionTable.join(automaticSVP, Seq("gid"), "left")
 
     //generate the case dimension and bridge tables
     val (caseDimensionDf, caseBridgeDf) = GenerateCaseDimensionTable(sparkSession, caseInputData, trapsDimensionTable)
